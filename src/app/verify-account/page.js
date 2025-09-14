3857cd5adc805af3826352014f163a04540d2fa4
cherrylnh17 <masluqman371@gmail.com>
2025-09-14 11:51:23 +0700

    perbaikan error

--- src/app/verify-account/page.js
+++ src/app/verify-account/page.js
@@ -1,12 +1,15 @@
 "use client";
 import OtpWrapper from "@/auth/OtpWrapper";
 import Navbar from "@/auth/Navbar";
+import { Suspense } from 'react';
 
 export default function Home() {
   return (
     <div className="bg-gradient-to-br from-pink-400/40 via-yellow-400/40 to-orange-400/40 text-white">
-        <Navbar />
-        <OtpWrapper />
+        <Suspense fallback={<div>Loading halaman OTP...</div>}>
+          <Navbar />
+          <OtpWrapper />
+        </Suspense>
     </div>
   );
 }
