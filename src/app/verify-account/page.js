2a2504e3019f7eea845800dd7d446a59653b08d3
cherrylnh17 <masluqman371@gmail.com>
2025-09-14 11:49:17 +0700

    error

--- src/app/verify-account/page.js
+++ src/app/verify-account/page.js
@@ -1,12 +1,12 @@
 "use client";
-import Otp from "../../auth/Otp";
-import Navbar from "../../auth/Navbar";
+import OtpWrapper from "@/auth/OtpWrapper";
+import Navbar from "@/auth/Navbar";
 
 export default function Home() {
   return (
     <div className="bg-gradient-to-br from-pink-400/40 via-yellow-400/40 to-orange-400/40 text-white">
         <Navbar />
-        <Otp />
+        <OtpWrapper />
     </div>
   );
 }
---
+++ src/auth/OtpWrapper.jsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import Otp from './Otp';
+
+export default function OtpWrapper() {
+  return <Otp />;
+}
