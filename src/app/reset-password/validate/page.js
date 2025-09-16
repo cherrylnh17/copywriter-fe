077a671dd6bcee65d6c31b34583aca03cbd226a4
cherrylnh17 <masluqman371@gmail.com>
2025-09-16 17:17:03 +0700

    gatau dah

--- src/app/reset-password/validate/page.js
+++ src/app/reset-password/validate/page.js
@@ -1,11 +1,14 @@
-import ValidatePassword from "@/auth/ValidatePassword";
-import Navbar from "@/auth/Navbar"
+import { Suspense } from "react";
+import Navbar from "@/auth/Navbar";
+import ValidatePassword from "./ValidatePassword"; 
 
 export default function Home() {
   return (
     <div className="bg-gradient-to-br from-pink-400/40 via-yellow-400/40 to-orange-400/40 text-white">
-        <Navbar />
+      <Navbar />
+      <Suspense fallback={<div>Loading...</div>}>
         <ValidatePassword />
+      </Suspense>
     </div>
   );
 }
